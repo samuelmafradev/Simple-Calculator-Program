b94e58558f66e3e89632ef39dc261352d56ae490
Samuel Mafra <samuelmafraf99@gmail.com>
2026-04-14 22:33:15 -0300

    Add files via upload

---
+++ CalculatorProgram/main.py
@@ -0,0 +1,45 @@
+# CALCULATOR PROGRAM.
+# ------------------ #
+# 1. First part. Logic Math.
+def calculate(n1, op, n2):
+    match op:
+        case '+': return n1 + n2
+        case '-': return n1 - n2
+        case '*': return n1 * n2
+        case '/': return n1 / n2 if n2 != 0 else "Can NOT be divided by zero."
+        case _: return "Invalid Operator."
+
+# 2. Calculate Logic.
+def run_calculator():
+    print("--- Calculator Program ---")
+    try:
+        num1 = float(input("First number: "))
+        oper = input("Operation: (+, -, /, *) ")
+        num2 = float(input("Second number: "))
+
+        # Call the logic.
+        result = calculate(num1, oper, num2)
+        print(f"The result is: {result}")
+
+    except ValueError:
+        print("Invalid input bro.")
+
+#3. Loop Function.
+if __name__ == "__main__":
+    while True:
+        run_calculator()
+        user_choice = input("\nDo you want another calculation? (y/n) ").lower()
+        if user_choice != 'y':
+            print("End of the program.")
+            break
+
+
+
+
+
+
+
+
+
+
+
